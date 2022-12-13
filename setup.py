9c52d8e9bb9c35d275fc06c4a8e910c8e4c2db05
Dan Philps <55665698+danphilps@users.noreply.github.com>
2022-12-13 22:03:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/danphilps/FinGov",
-    packages=find_packages('GovernanceUtils'),
+    packages=find_packages('GovernanceUtils.py'),
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
