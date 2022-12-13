3dab080e56069af484511f882fc3b85158df2b2e
Dan Philps <55665698+danphilps@users.noreply.github.com>
2022-12-13 21:39:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -95,37 +95,13 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"], include=['GovernanceUtils','FairnessUtils','StakeholderKPIReporting']),
-    # If your package is a single module, use this instead of 'packages':
-    py_modules=['GovernanceUtils.py'], #, 'FairnessUtils.py', 'StakeholderKPIReporting.py'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    version=VERSION,
+    packages=find_packages(include=['GovernanceUtils.*', 'GovernanceUtils']),
+    install_requires=[
+        'PyYAML',
+        'pandas==0.23.3',
+        'numpy>=1.14.5',
+        'matplotlib>=2.2.0,,
+        'jupyter'
+    ]
 )
