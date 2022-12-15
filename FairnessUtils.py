7a53602dbb67dc8506f0399cd338efa97d947bbb
Dan Philps <55665698+danphilps@users.noreply.github.com>
2022-12-15 08:54:56 +0000

    Update FairnessUtils.py

--- FairnessUtils.py
+++ FairnessUtils.py
@@ -424,12 +424,11 @@ class FairnessUtils():
         ax[i].clear()
         
         #The plot displays a range that is +/- 20% from the metric for the majority class
-        if ametric == fairness_metric:
-          ax[i].axhline(y=majority_class_metric.values[0]*0.8,color='red', label='Upper unfairness bound')
-          ax[i].axhline(y=majority_class_metric.values[0],color='green', label='Fairness parity')
-          ax[i].axhline(y=majority_class_metric.values[0]*1.2,color='red', label='Lower unfairness bound')
-          ax[i].legend(loc='lower center')
-        
+        ax[i].axhline(y=majority_class_metric.values[0]*0.8,color='red', label='Upper unfairness bound')
+        ax[i].axhline(y=majority_class_metric.values[0],color='green', label='Fairness parity')
+        ax[i].axhline(y=majority_class_metric.values[0]*1.2,color='red', label='Lower unfairness bound')
+        ax[i].legend(loc='lower center')
+
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax[i].set_ylabel('%')
         ax[i].set_xticklabels(X_val, rotation = 45, ha="right")
