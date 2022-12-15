cdd57d44cd8da928eb89601bee8f727622871ea2
Dan Philps <55665698+danphilps@users.noreply.github.com>
2022-12-15 07:26:15 +0000

    Update CFACreditUseCaseUtils.py

--- CFACreditUseCaseUtils.py
+++ CFACreditUseCaseUtils.py
@@ -10,11 +10,11 @@ def train_test_split_deterministic(X: pd.DataFrame,
   # Split the data at this point.
   train_row_end = int(test_size * X.shape[0])
 
-  X_train = X.iloc[0:train_row_end,:]
-  y_train = y.iloc[0:train_row_end]
+  X_test = X.iloc[0:train_row_end,:]
+  y_test = y.iloc[0:train_row_end]
 
-  X_test = X.iloc[train_row_end+1:,:]
-  y_test = y.iloc[train_row_end+1:]
+  X_train = X.iloc[train_row_end+1:,:]
+  y_train = y.iloc[train_row_end+1:]
   
   return X_train, X_test, y_train, y_test
   
